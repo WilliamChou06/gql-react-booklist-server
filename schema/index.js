11bbd401aa1bb1c6e4afb2c30a4dbeb33aba9ec2
William Chou <williamchoudev@gmail.com>
2019-05-17 10:07:11 -0400

    Add GraphQL boilerplate for author and book types

--- schema/index.js
+++ schema/index.js
@@ -12,7 +12,41 @@ const {
 const BookType = new GraphQLObjectType({
   name: 'Book',
   fields: () => ({
-    id: { type :GraphQLID},
-    name: { type :GraphQLString}
+    id: { type: GraphQLID },
+    title: { type: GraphQLString },
+    authors: new GraphQLList(AuthorType)
   })
+});
+
+const AuthorType = new GraphQLObjectType({
+  name: 'Author',
+  fields: () => ({
+    id: { type: GraphQLID },
+    name: { type: GraphQLString },
+    books: new GraphQLList(BookType)
+  })
+});
+
+const RootQuery = new GraphQLObjectType({
+  name: 'RootQuery',
+  fields: {
+    book: {
+      type: BookType,
+      args: { id: GraphQLID },
+      resolve(parent, args){
+
+      }
+    },
+    author: {
+      type: AuthorType,
+      args: { id: GraphQLID },
+      resolve(parent, args){
+
+      }
+    }
+  }
+})
+
+module.exports = new GraphQLSchema({
+  query: RootQuery
 })
